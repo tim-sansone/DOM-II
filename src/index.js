e5dea025b0c3747c142bee3a877c623df0b08e6d
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-05-11 13:50:39 -0500

    through EL #3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './less/index.less'
 
-// Your code goes here!
+
+/* -------------------- #1 -------------------------*/
+
+const navLinks = document.querySelectorAll(".nav-link");
+// console.log(nav);
+
+navLinks.forEach(each => {
+    each.addEventListener("mouseover", () => {
+        each.style.color = "white"
+    })
+    each.addEventListener("mouseout", () => {
+        each.style.color = "#212529";
+    })
+})
+
+/* -------------------- #2 -------------------------*/
+
+const headerTitle = document.querySelector("header h1");
+
+headerTitle.addEventListener("click", () => {
+   headerTitle.classList.toggle("big-red");
+    // headerTitle.style.color = "red";
+    // headerTitle.style.fontSize = "10rem";
+})
+
